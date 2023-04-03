22cf0b682e3cb27ac2eb18d1263b71f073d6efe2
Mickey Camarco <48422179+mcamarco@users.noreply.github.com>
2023-04-03 17:08:43 -0400

    debugging JS

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,25 @@
 console.log(window);
-
 var APIKey = "383165bf31a4af053f333f929519989c";
-var city="New York";
+var city = "Philadelphia";
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+var responseText = document.getElementById('response-text');
 
-fetch(queryURL);
+fetch(queryURL)
+    .then(function (response) {
+        return response.json()
+    })
+
+function getApi(queryURL) {
+    fetch(queryURL)
+        .then(function (response) {
+            console.log(response);
+            if (response.status === 200) {
+                responseText.textContent = response.status;
+            }
+            return response.json();
+        });
+};
+
+console.log(response);
+//   getApi(requestUrl);
 
