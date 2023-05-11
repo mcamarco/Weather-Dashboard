582a128ed14f58df4228f8af75dfe8a8420946bb
Mickey Camarco <48422179+mcamarco@users.noreply.github.com>
2023-05-11 18:52:04 -0400

    got 5-day forecast to load but wrong amount of data is being appended

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,13 +1,26 @@
-var APIKey = "62f12928e4bb4b706baabee5c050c6f0";
-var storedData = JSON.parse(localStorage.getItem('myAppData')) || [];
-var recentSearches = storedData.recentSearches || [];
+var APIKey = "086f9d9aa37aee06fc531999dfe497b6";
+var recentSearches = JSON.parse(localStorage.getItem('myAppData')) || [];
+// var recentSearches = storedData.recentSearches || [];
 
-// event listener for city name submit
+// event listener for city name once submitted
 console.log("cityNameSubmit")
+$("#language-buttons").on("click", ".btn", function () {
+    var city = $(this).text();
+    getCoord(city);
+});
 $("#submit").on("click", function (e) {
     e.preventDefault();
 
     var city = $("#cityName").val();
+    var cityNameButton = $("<button>");
+    cityNameButton.attr("class", "btn");
+    cityNameButton.attr("id", "cityNamesubmit");
+    cityNameButton.text(city);
+    $("#language-buttons").prepend(cityNameButton);
+    cityNameButton.on("click", function () {
+        getSearchHistoryBtn($(this).text());
+    });
+
     getCoord(city);
 })
 
@@ -22,27 +35,52 @@ function getCoord(city) {
             var lat = data[0].lat;
             var lon = data[0].lon;
             currentWeather(lat, lon);
-            // forecastWeather(lat,lon);
+            forecastWeather(lat, lon);
 
             // add new city to recent searches array
             recentSearches.unshift(city);
             if (recentSearches.length > 5) {
                 recentSearches.pop();
             }
-            localStorage.setItem('myAppData', JSON.stringify({ recentSearches: recentSearches }));
+            localStorage.setItem('myAppData', JSON.stringify(recentSearches));
 
-            // update buttons for recent searches
-            $("#language-buttons").empty();
-            for (var i = 0; i < recentSearches.length; i++) {
-                var cityNameButton = $("<button>");
-                cityNameButton.attr("class", "btn");
-                cityNameButton.attr("id", "cityNamesubmit");
-                cityNameButton.text(recentSearches[i]);
-                $("#language-buttons").append(cityNameButton);
-            }
-
-        })
+        });
 }
+
+function getSearchHistoryBtn(city) {
+    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${APIKey}`)
+        .then(function (res) {
+            return res.json()
+        })
+        .then(function (data) {
+            var lat = data[0].lat;
+            var lon = data[0].lon;
+            currentWeather(lat, lon);
+            forecastWeather(lat, lon);
+
+            // add new city to recent searches array
+            recentSearches.unshift(city);
+            if (recentSearches.length > 5) {
+                recentSearches.pop();
+            }
+            localStorage.setItem('myAppData', JSON.stringify(recentSearches));
+
+        });
+}
+// append recent searches to page as buttons - on page load from local storage
+for (var i = 0; i < recentSearches.length; i++) {
+    var cityNameButton = $("<button>");
+    cityNameButton.attr("class", "btn");
+    cityNameButton.attr("id", "cityNamesubmit");
+    cityNameButton.text(recentSearches[i]);
+    $("#language-buttons").append(cityNameButton);
+
+    // Add event listener to the button
+    cityNameButton.on("click", function () {
+        getCoord($(this).text());
+    });
+}
+
 
 // get current weather
 console.log("currentWeather")
@@ -74,41 +112,42 @@ function currentWeather(lat, lon) {
         })
 }
 
-
-// TODO: Forecast weather
+// Forecast weather
 console.log("forecastWeather")
-// function forecastWeather(lat, lon)
-// {
-//     fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly&units=imperial&appid=${APIKey}`)
-//     .then(function(res){
-//         return res.json();
-//      })
-//     .then(function(data){
-//         var pcityname = $("<h2>");
-//         pcityname.text(data.name);
-//         $("#current").append(pcityname);
+function forecastWeather(lat, lon) {
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}&units=imperial`)
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (data) {
+            console.log(data)
+            for (var i = 0; i < data.list.length; i += 8) {
 
-//         var pTemp = $("<p>");
-//         pTemp.text("Temperature: " + data.main.temp + " °F");
-//         $("#current").append(pTemp);
+                // TODO: Cleanup Date Formatting
+                var dateToday = $("<h2>");
+                var date = new Date(data.list[i].dt_txt);
+                var options = { weekday: 'long', month: 'long', day: 'numeric' };
+                var formattedDate = date.toLocaleDateString(undefined, options);
+                dateToday.text(formattedDate);
+                $("#days").append(dateToday);
 
-//         var pFeelsLike = $("<p>");
-//         pFeelsLike.text("Feels like: " + data.main.feels_like + " °F");
-//         $("#current").append(pFeelsLike);
+                var forecastIcon = $("<img>").attr("src", "https://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png");
+                $("#days").append(forecastIcon);
+                console.log(data.list[i].weather[0].icon)
 
-//         var pHum = $("<p>");
-//         pHum.text("Humidity: " + data.main.humidity + " %");
-//         $("#current").append(pHum);
+                var pTemp = $("<p>");
+                pTemp.text("Temperature: " + data.list[i].main.temp + " °F");
+                $("#days").append(pTemp);
 
-//     })
-// }
+                var pFeelsLike = $("<p>");
+                pFeelsLike.text("Feels like: " + data.list[i].main.feels_like + " °F");
+                $("#days").append(pFeelsLike);
 
-// append recent searches to page
-for (var i = 0; i < recentSearches.length; i++) {
-    var cityNameButton = $("<button>");
-    cityNameButton.attr("class", "btn");
-    cityNameButton.attr("id", "cityNamesubmit");
-    cityNameButton.text(recentSearches[i]);
-    $("#language-buttons").append(cityNameButton);
+                var pHum = $("<p>");
+                pHum.text("Humidity: " + data.list[i].main.humidity + " %");
+                $("#days").append(pHum);
+            }
+        })
 }
 
+// TODO: ICONS FOR WEATHER
