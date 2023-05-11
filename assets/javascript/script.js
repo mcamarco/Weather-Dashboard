13f0d5611b5601d82c0f27f7d208c410bdd9283b
Mickey Camarco <48422179+mcamarco@users.noreply.github.com>
2023-05-11 16:28:49 -0400

    re-did CSS, adjusting script.js, editing forecast

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,11 +1,10 @@
-var APIKey = "086f9d9aa37aee06fc531999dfe497b6";
+var APIKey = "62f12928e4bb4b706baabee5c050c6f0";
 var storedData = JSON.parse(localStorage.getItem('myAppData')) || [];
+var recentSearches = storedData.recentSearches || [];
 
-
-console.log(window);
-
-// TODO: event listener for city name submit
-$("#submit").on("click", function(e){
+// event listener for city name submit
+console.log("cityNameSubmit")
+$("#submit").on("click", function (e) {
     e.preventDefault();
 
     var city = $("#cityName").val();
@@ -13,61 +12,103 @@ $("#submit").on("click", function(e){
 })
 
 // get coordinates for city
-function getCoord(city)
-{
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${APIKey}`)
-    .then(function(res){
-       return res.json();
-    })
-    .then(function(data){
-        //console.log(data);
-        var lat = data[0].lat;
-        var lon = data[0].lon;
-        currentWeather(lat, lon);
-        forecastWeather(lat,lon);
-        
-        
-    })
+console.log("getCoord")
+function getCoord(city) {
+    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${APIKey}`)
+        .then(function (res) {
+            return res.json()
+        })
+        .then(function (data) {
+            var lat = data[0].lat;
+            var lon = data[0].lon;
+            currentWeather(lat, lon);
+            // forecastWeather(lat,lon);
+
+            // add new city to recent searches array
+            recentSearches.unshift(city);
+            if (recentSearches.length > 5) {
+                recentSearches.pop();
+            }
+            localStorage.setItem('myAppData', JSON.stringify({ recentSearches: recentSearches }));
+
+            // update buttons for recent searches
+            $("#language-buttons").empty();
+            for (var i = 0; i < recentSearches.length; i++) {
+                var cityNameButton = $("<button>");
+                cityNameButton.attr("class", "btn");
+                cityNameButton.attr("id", "cityNamesubmit");
+                cityNameButton.text(recentSearches[i]);
+                $("#language-buttons").append(cityNameButton);
+            }
+
+        })
 }
 
-// current weather
-function currentWeather(lat, lon)
-{
+// get current weather
+console.log("currentWeather")
+function currentWeather(lat, lon) {
+    $("#current").empty();
+
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${APIKey}`)
-    .then(function(res){
-        return res.json();
-     })
-     .then(function(data){
-         console.log(data);
-         var pHum = $("<p>");
-         pHum.text("Humidity: " + data.main.humidity);
-         $("#current").append(pHum);
-     })
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (data) {
+            var pcityname = $("<h3>");
+            pcityname.text(data.name);
+            $("#current").append(pcityname);
+
+            var pTemp = $("<p>");
+            pTemp.text("Temperature: " + data.main.temp + " °F");
+            $("#current").append(pTemp);
+
+            var pFeelsLike = $("<p>");
+            pFeelsLike.text("Feels like: " + data.main.feels_like + " °F");
+            $("#current").append(pFeelsLike);
+
+            var pHum = $("<p>");
+            pHum.text("Humidity: " + data.main.humidity + " %");
+            $("#current").append(pHum);
+
+
+        })
 }
 
-// forecast weather
-function forecastWeather(lat, lon)
-{
-    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly&units=imperial&appid=${APIKey}`)
-    .then(function(res){
-        return res.json();
-     })
-     .then(function(data){
-         console.log(data);
-         // TODO: append forecast data to HTML
-     })
+
+// TODO: Forecast weather
+console.log("forecastWeather")
+// function forecastWeather(lat, lon)
+// {
+//     fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly&units=imperial&appid=${APIKey}`)
+//     .then(function(res){
+//         return res.json();
+//      })
+//     .then(function(data){
+//         var pcityname = $("<h2>");
+//         pcityname.text(data.name);
+//         $("#current").append(pcityname);
+
+//         var pTemp = $("<p>");
+//         pTemp.text("Temperature: " + data.main.temp + " °F");
+//         $("#current").append(pTemp);
+
+//         var pFeelsLike = $("<p>");
+//         pFeelsLike.text("Feels like: " + data.main.feels_like + " °F");
+//         $("#current").append(pFeelsLike);
+
+//         var pHum = $("<p>");
+//         pHum.text("Humidity: " + data.main.humidity + " %");
+//         $("#current").append(pHum);
+
+//     })
+// }
+
+// append recent searches to page
+for (var i = 0; i < recentSearches.length; i++) {
+    var cityNameButton = $("<button>");
+    cityNameButton.attr("class", "btn");
+    cityNameButton.attr("id", "cityNamesubmit");
+    cityNameButton.text(recentSearches[i]);
+    $("#language-buttons").append(cityNameButton);
 }
-
-
-//     // push zipcode to local storage
-//     storedData.unshift(zipCode);
-//     localStorage.setItem('myAppData', JSON.stringify(storedData));
-//     zipButtons();
-// });
-
-
-
-// parse data from Array
-
-// set data to points on HTML
 
